67451194a6fd0e0829ff83dc81cf85c3e3f16954
Adéla Šterberová <asterberova@gmail.com>
2021-12-01 15:47:24 +0100

    Change from partial_fit to fit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mnist import MNIST
 
 from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.utils import shuffle
 
 from hyperopt.pyll import scope
 from hyperopt import hp, tpe, fmin, Trials, STATUS_OK
@@ -34,7 +35,6 @@ print("Target test Data Shape", len(y_test))
 #     mnist.data, mnist.target, test_size=1/7.0, random_state=0)
 
 
-
 # define space for TPE HPO
 space = {"lrate": hp.uniform("lrate", 0, 1),
          "l2_reg": hp.uniform("l2_reg", 0, 1),
@@ -42,7 +42,7 @@ space = {"lrate": hp.uniform("lrate", 0, 1),
          "n_epochs": scope.int(hp.quniform("n_epochs", 5, 2000, 1))}
 
 
-def log_reg_acc(params):
+def log_reg_acc_partial(params):
 
     clf = SGDClassifier(loss='log',
                         learning_rate='constant',
@@ -51,13 +51,30 @@ def log_reg_acc(params):
                         l1_ratio=params['l2_reg'])
 
     for n in range(params['n_epochs']):
-        n_batches = len(X_train) // params['batchsize']
+        training_X, training_y = shuffle(X_train, y_train, random_state=n)
+        # print('n_epochs: {}/{}'.format(n, params['n_epochs']))
+        n_batches = len(training_X) // params['batchsize']
         for minibatch_idx in range(n_batches):
-            # for minibatch_idx in range(1):
             clf.partial_fit(
-                X_train[minibatch_idx * params['batchsize']: (minibatch_idx + 1) * params['batchsize']],
-                y_train[minibatch_idx * params['batchsize']: (minibatch_idx + 1) * params['batchsize']],
+                training_X[minibatch_idx * params['batchsize']: (minibatch_idx + 1) * params['batchsize']],
+                training_y[minibatch_idx * params['batchsize']: (minibatch_idx + 1) * params['batchsize']],
                 classes=np.unique(y_train))
+
+    acc = clf.score(X_test, y_test)
+    return acc
+
+
+def log_reg_acc(params):
+
+    clf = SGDClassifier(loss='log',
+                        learning_rate='constant',
+                        eta0=params['lrate'],
+                        penalty='elasticnet',
+                        l1_ratio=params['l2_reg'],
+                        max_iter=params['n_epochs'],
+                        shuffle=True)
+
+    clf.fit(X_train, y_train)
 
     acc = clf.score(X_test, y_test)
     return acc
@@ -69,48 +86,39 @@ def obj_func(params):
     return {'loss': loss, 'status': STATUS_OK}
 
 
-trials = Trials()
-best_params = fmin(fn=obj_func,
-                space=space,
-                algo=tpe.suggest,
-                max_evals=2,
-                trials=trials)
+if __name__ == "__main__":
+    num_repeat = 10
 
-print("Best parameters:",best_params)
-print(trials.best_trial['result']['loss'])
+    for i in range(num_repeat):
+        print(f'Run {i}/{num_repeat}')
+        # perform TPE optimization and do logging
+        trials = Trials()
+        best_params = fmin(fn=obj_func,
+                        space=space,
+                        algo=tpe.suggest,
+                        max_evals=100,
+                        trials=trials)
 
-# pickle.dump(trials, open("trials.json", "wb"))
+        print("Best parameters:",best_params)
+        print(trials.best_trial['result']['loss'])
 
+        loss = trials.losses()
+        val = trials.vals
+        val['loss'] = loss
+        print(val)
 
-loss = trials.losses()
-val = trials.vals
-val['loss'] = loss
+        # with open('best.json', 'w') as f:
+        #     f.write(json.dumps({"Loss": trials.best_trial['result']['loss'],
+        #                         "Best params": best_params}))
 
-print(val)
-print(loss)
-
-# arr_zip = zip(*(val['lrate'], val['l2_reg'], val['batchsize'], val['n_epochs'], loss))
-
-
-with open('dominostats.json', 'w') as f:
-    f.write(json.dumps({"Loss": trials.best_trial['result']['loss'],
-                        "Best params" : best_params}))
-
-
-filename = 'test.csv'
-header = ['lrate', 'l2_reg', 'batchsize', 'n_epochs', 'loss']
-values = (val.get(key, []) for key in header)
-data = (dict(zip(header, row)) for row in zip(*values))
-with open(filename, 'w') as f:
-    #
-    # writer = csv.writer(f)
-    # writer.writerow(val.keys())
-    # writer.writerows(zip(*val.values()))
-
-
-    wrtr = csv.DictWriter(f, fieldnames=header)
-    wrtr.writeheader()
-    wrtr.writerows(data)
+        filename = 'csv_data/hpo{}.csv'.format(i)
+        header = ['lrate', 'l2_reg', 'batchsize', 'n_epochs', 'loss']
+        values = (val.get(key, []) for key in header)
+        data = (dict(zip(header, row)) for row in zip(*values))
+        with open(filename, 'w') as f:
+            wrtr = csv.DictWriter(f, fieldnames=header)
+            wrtr.writeheader()
+            wrtr.writerows(data)
 
 
 
